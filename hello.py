085e8b00d3ddf3d75770c3f064a3eb9639c72d48
mikespaans <89783541+mikespaans@users.noreply.github.com>
2021-09-06 15:32:23 +0200

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,10 @@
+print('Hello wereld van…                                    
+
+    pppp    y     y   ttttttt   h   h    oooo   nnnn          
+    p   p    y   y       t      h   h   o    o  n   n
+    p   p     y y        t      h   h   o    o  n   n
+    pppp       y         t      hhhhh   o    o  n   n
+    p          y         t      h   h   o    o  n   n
+    p          y         t      h   h   o    o  n   n
+    p          y         t      h   h    oooo   n   n')
+    #Mike Spaans studentennummer: 99069233
