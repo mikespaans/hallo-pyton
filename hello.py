0be4f19ba410b1d65552e2fe462be546be74ab7d
Mike Spaans <99069233@mydavinci.nl>
2021-10-06 09:37:27 +0200

    hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,8 @@
-print('Hello wereld van…                                    
-
-    pppp    y     y   ttttttt   h   h    oooo   nnnn          
-    p   p    y   y       t      h   h   o    o  n   n
-    p   p     y y        t      h   h   o    o  n   n
-    pppp       y         t      hhhhh   o    o  n   n
-    p          y         t      h   h   o    o  n   n
-    p          y         t      h   h   o    o  n   n
-    p          y         t      h   h    oooo   n   n')
-    #Mike Spaans studentennummer: 99069233
+print("Hello wereld van…")
+print(" pppp    y     y   ttttttt   h   h    oooo   nnnn")
+print(" p   p    y   y       t      h   h   o    o  n   n")
+print(" p   p     y y        t      h   h   o    o  n   n")
+print("pppp       y         t      hhhhh   o    o  n   n")
+print("p          y         t      h   h   o    o  n   n")
+print("p          y         t      h   h   o    o  n   n")
+print("p          y         t      h   h    oooo   n   n")
